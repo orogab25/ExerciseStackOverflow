dc5b09ee6defc3b6c61bfa2cce571ccc3029e133
orogab25 <orogab25@gmail.com>
2020-03-24 13:41:19 +0100

    Add project files.

---
+++ ExerciseStackOverflow/Post.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExerciseStackOverflow
+{
+    class Post
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public DateTime Date { get; set; }
+        public int Vote { get; private set; }
+
+        public Post()
+        {
+            Vote = 0;
+        }
+
+        public Post(string title, string description, DateTime date):this()
+        {
+            Title = title;
+            Description = description;
+            Date = date;
+        }
+
+        public void UpVote()
+        {
+            Vote++;
+        }
+
+        public void DownVote()
+        {
+            Vote--;
+        }
+
+        public void printPost()
+        {
+            Console.WriteLine("Title: "+Title+"\nDescription: "+Description+"\nCreated: "+Date+"\nVote: "+Vote+"\n");
+        }
+    }
+}
---
+++ ExerciseStackOverflow/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ExerciseStackOverflow
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Post post = new Post("SQL error","Can't update table",DateTime.Now);
+            post.printPost();
+            post.UpVote();
+            Console.WriteLine("Vote: " + post.Vote);
+            post.DownVote();
+            Console.WriteLine("Vote: " + post.Vote);
+        }
+    }
+}
